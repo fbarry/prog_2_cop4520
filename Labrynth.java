cda3e5ef6db1bab54c936636f653c4d31022be29
Fiona Barry <55502851+fbarry@users.noreply.github.com>
2022-03-03 17:08:53 -0500

    Assignment completion

---
+++ Labrynth.java
@@ -0,0 +1,84 @@
+import java.util.*;
+
+public class Labrynth {
+
+    // Random number generator
+    static final Random RAND = new Random();
+
+    // Number of guests
+    static final int N = 1_000_000;
+
+    // Keep track of who visited
+    static int visitedCount = 0;
+    static boolean[] visited = new boolean[N];
+
+    // Keep track of who reported
+    static int reportedCount = 0, totalNumNeeded = 0;
+    static boolean[] reported = new boolean[N];
+
+    public static void main(String[] args) {
+
+        // Init Threads
+        Thread[] threads = new Thread[N];
+        for (int i = 0; i < N; i++)
+            threads[i] = new Thread(new GuestThread(i));
+
+        // Start time
+        long clock = System.currentTimeMillis();
+
+        // While not everyone has reported
+        while (reportedCount < N) {
+            // Minotaur chooses next guest
+            int nextGuest = RAND.nextInt(N);
+            // If this guest hasn't already been chosen to enter
+            // (kind of like being in a line to enter but really
+            // it's just the minotaur working proactively)
+            // Then allow them to enter
+            if (!threads[nextGuest].isAlive())
+                threads[nextGuest].run();
+        }
+
+        System.out.println("Total Number of Labrynth Visits: " + totalNumNeeded +
+                " for " + N + " people in " +
+                (System.currentTimeMillis() - clock) + " ms.");
+    }
+
+    // This is where each guest visits the labrynth
+    // It changes global data so it's synchronized... the resources are locked
+    // while another thread is accessing them (i.e. one guest in the lab at once)
+    static synchronized void visitLabrynth(int index) {
+        // Increase total num of guest visits
+        totalNumNeeded++;
+
+        // If they haven't visited yet, they leave a wrapper behind
+        if (!visited[index]) {
+            visited[index] = true;
+            visitedCount++;
+        }
+
+        // If this guest can see all N wrappers on the ground,
+        // They report to the minotaur that all guests have visited
+        if (visitedCount >= N && !reported[index]) {
+            reported[index] = true;
+            reportedCount++;
+        }
+    }
+
+    // GuestThead object
+    static class GuestThread implements Runnable {
+
+        // Every guest has an index
+        private int index;
+
+        // Constructor
+        public GuestThread(int i) {
+            this.index = i;
+        }
+
+        @Override
+        public void run() {
+            // Visit the labrynth
+            visitLabrynth(index);
+        }
+    }
+}
---
+++ Vase.java
@@ -0,0 +1,147 @@
+import java.util.*;
+
+public class Vase {
+
+    // Random number generator
+    static final Random RAND = new Random();
+
+    // Number of guests
+    static final int N = 1_000_000;
+
+    // The "Busy" sign / is there someone there
+    static boolean isBusy = false;
+
+    // Number of guests visited
+    static int visitedCount = 0;
+    static boolean[] visited = new boolean[N];
+
+    // Keep track of num visits per guest for analysis
+    static int[] totalVisitsPerGuest = new int[N];
+
+    public static void main(String[] args) {
+
+        // Init Threads
+        Thread[] threads = new Thread[N];
+        for (int i = 0; i < N; i++)
+            threads[i] = new Thread(new GuestThread(i));
+
+        // Init analysis data
+        double averageNumVisits = 0;
+        long clock = System.currentTimeMillis();
+
+        // Strategy 1: Random Visits
+        while (visitedCount < N) {
+            // This guest wants to visit
+            int nextGuest = RAND.nextInt(N);
+            // If they're already visiting then forget it
+            if (!threads[nextGuest].isAlive()) {
+                // They try the door
+                totalVisitsPerGuest[nextGuest]++;
+                // If no one is there, they can see the vase
+                if (!isBusy) {
+                    threads[nextGuest].run();
+                }
+            }
+        }
+
+        // Calculate average
+        for (int i = 0; i < N; i++)
+            averageNumVisits += totalVisitsPerGuest[i];
+
+        // Print stats
+        System.out.println("\n\nStrategy 1:");
+        System.out.println("Execution Time: " + (System.currentTimeMillis() - clock));
+        System.out.println("Average Visits: " + (averageNumVisits / N));
+
+        // Reset data
+        resetGlobal();
+        averageNumVisits = 0;
+        clock = System.currentTimeMillis();
+
+        // Strategy 2: Status Sign
+        while (visitedCount < N) {
+            // This guest wants to visit
+            int nextGuest = RAND.nextInt(N);
+            // If the sign says available and they're not currently running, they go try
+            if (!isBusy && !threads[nextGuest].isAlive()) {
+                totalVisitsPerGuest[nextGuest]++;
+                threads[nextGuest].run();
+            }
+        }
+
+        // Calculate average
+        for (int i = 0; i < N; i++)
+            averageNumVisits += totalVisitsPerGuest[i];
+
+        // Print stats
+        System.out.println("\n\nStrategy 2:");
+        System.out.println("Execution Time: " + (System.currentTimeMillis() - clock));
+        System.out.println("Average Visits: " + (averageNumVisits / N));
+
+        // Reset data
+        resetGlobal();
+        averageNumVisits = 0;
+        clock = System.currentTimeMillis();
+
+        // Strategy 3: Queue
+        while (visitedCount < N) {
+            // This guest wants to get in line
+            int nextGuest = RAND.nextInt(N);
+            // If they're not in line, get in line
+            if (!threads[nextGuest].isAlive()) {
+                totalVisitsPerGuest[nextGuest]++;
+                threads[nextGuest].run();
+            }
+        }
+
+        // Calculate average
+        for (int i = 0; i < N; i++)
+            averageNumVisits += totalVisitsPerGuest[i];
+
+        // Print stats
+        System.out.println("\n\nStrategy 3:");
+        System.out.println("Execution Time: " + (System.currentTimeMillis() - clock));
+        System.out.println("Average Visits: " + (averageNumVisits / N));
+    }
+
+    // The synchronized method of visiting the vase ensures only one guest can visit at once
+    static synchronized void visitVase(int index) {
+        // A guest is visiting
+        isBusy = true;
+
+        // If it's their first time, mark it
+        if (!visited[index]) {
+            visited[index] = true;
+            visitedCount++;
+        }
+
+        // They're done
+        isBusy = false;
+    }
+
+    // GuestThread is each guest
+    static class GuestThread implements Runnable {
+
+        // Each guest has an index
+        private int index;
+
+        // Constructor
+        public GuestThread(int i) {
+            this.index = i;
+        }
+
+        @Override
+        public void run() {
+            // Visit the vase
+            visitVase(this.index);
+        }
+    }
+
+    // Resets global data for next trial
+    static void resetGlobal() {
+        visitedCount = 0;
+        isBusy = false;
+        visited = new boolean[N];
+        totalVisitsPerGuest = new int[N];
+    }
+}
